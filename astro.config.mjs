f13cc5a7dfbeb02ef9c52f02a3a7652e426f8a93
marica1102 <misato.gold@gmail.com>
2025-03-13 21:40:56 +0900

    Update .gitignore to exclude 'dist/' directory, change base URL in astro.config.mjs, and add GitHub Actions workflow for deployment to GitHub Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marica1102.github.io',
-  base: '/studying-astro-portfolio',
+  base: '/course-astro-main',
   integrations: [mdx(), sitemap(), icon()],
 })
